7206bd59463e36e4dc9ec5600e5bb44a8d066a6d
Zacarias Guilherme João <zacariasguilhermejoao@gmail.com>
2026-09-15 11:00:00 +0100

    feat: configure SplashScreen plugin (black bg, full screen, 2.5s)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,6 +7,18 @@ const config: CapacitorConfig = {
   bundledWebRuntime: false,
   server: {
     androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2500,
+      launchAutoHide: true,
+      backgroundColor: '#000000',
+      androidSplashResourceName: 'splash',
+      androidScaleType: 'CENTER_CROP',
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true
+    }
   }
 };
 

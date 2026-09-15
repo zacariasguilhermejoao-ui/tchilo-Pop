c9b4c678196034667240120af3ca891bac972eb9
Zacarias Guilherme João <zacariasguilhermejoao@gmail.com>
2026-09-15 08:19:22 +0100

    Fix Capacitor web directory

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.tchilo.isabstudio',
   appName: 'tchilo-Pop',
-  webDir: '.',
+  webDir: 'www',
   bundledWebRuntime: false,
   server: {
     androidScheme: 'https'

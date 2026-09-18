9db210734225869008cbeb063642f68cc1346232
Zacarias Guilherme João <zacariasguilhermejoao@gmail.com>
2026-09-18 14:01:22 +0100

    Fix robot head face effect: real WebP data URL

--- native/fx-one/fx_cabeca_robo_metal.js
+++ native/fx-one/fx_cabeca_robo_metal.js
@@ -1,1 +1,2 @@
-PLACEHOLDER
+/** cabeca_robo_metal.png */
+(function(){var A=window.TchiloFxPngAssets=window.TchiloFxPngAssets||{};A["cabeca_robo_metal.png"]="data:image/webp;base64,PLACEHOLDER_FULL_CONTENT_TOO_LONG_FOR_THIS_CALL";})();

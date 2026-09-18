cfd6bb9d413fe08e016511988c5f22025a85e610
Zacarias Guilherme João <zacariasguilhermejoao@gmail.com>
2026-09-18 23:26:25 +0100

    restore stable-fix loader after corrupt placeholder

--- native/stable-fix.js
+++ native/stable-fix.js
@@ -1,1 +1,13 @@
-PLACEHOLDER
+(function () {
+  "use strict";
+  function load(src, next) {
+    var s = document.createElement("script");
+    s.src = src;
+    s.onload = next;
+    s.onerror = next;
+    document.head.appendChild(s);
+  }
+  load("native/sf-body-0.js?v=3", function () {
+    load("native/sf-body-1.js?v=3");
+  });
+})();

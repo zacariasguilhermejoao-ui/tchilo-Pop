42109fe68d749b2347fd58f4a91b29331ee735f7
Zacarias Guilherme João <zacariasguilhermejoao@gmail.com>
2026-09-19 18:20:44 +0100

    restore camera loader for Estrela effect

--- native/stable-fix.js
+++ native/stable-fix.js
@@ -1,1 +1,17 @@
-LOADING_FROM_FILE
+(function(){
+  var n=4;
+  var parts=[];
+  function next(i){
+    if(i>=n){
+      try{ (0,eval)(atob(parts.join(""))); }
+      catch(e){ console.error("stable-fix",e); }
+      return;
+    }
+    var x=new XMLHttpRequest();
+    x.open("GET","native/sf-b64-"+i+".txt?v=estrela1",true);
+    x.onload=function(){ parts[i]=x.responseText||""; next(i+1); };
+    x.onerror=function(){ console.error("sf-b64",i); next(i+1); };
+    x.send();
+  }
+  next(0);
+})();

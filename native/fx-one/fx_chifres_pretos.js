30c30b1b014ddc99bc61e4be8ee0973dca09f05a
Zacarias Guilherme João <zacariasguilhermejoao@gmail.com>
2026-09-19 21:18:04 +0100

    Add estrela, corrente, chifres effect assets

---
+++ native/fx-one/fx_chifres_pretos.js
@@ -0,0 +1,1 @@
+(function(){window.TchiloFxPngAssets=window.TchiloFxPngAssets||{};window.TchiloFxPngAssets['chifres-pretos.png']='data:image/webp;base64,PLACEHOLDER';})();
---
+++ native/fx-one/fx_colar_corrente_dourado.js
@@ -0,0 +1,1 @@
+(function(){window.TchiloFxPngAssets=window.TchiloFxPngAssets||{};window.TchiloFxPngAssets['colar-corrente-dourado.png']='data:image/webp;base64,PLACEHOLDER';})();

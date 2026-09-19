1261b069a269b21e11a1cd8b158b575d9b472816
Zacarias Guilherme João <zacariasguilhermejoao@gmail.com>
2026-09-19 21:24:49 +0100

    Add colar corrente effect assets (parts)

---
+++ native/fx-one/fx_colar_asm.js
@@ -0,0 +1,6 @@
+(function(){
+  window.TchiloFxPngAssets=window.TchiloFxPngAssets||{};
+  if(window.__colar_0&&window.__colar_1&&window.__colar_2){
+    window.TchiloFxPngAssets['colar-corrente-dourado.png']='data:image/webp;base64,'+window.__colar_0+window.__colar_1+window.__colar_2;
+  }
+})();

dbee94e250e6cd6125f361ed8653a796b75b4014
Zacarias Guilherme João <zacariasguilhermejoao@gmail.com>
2026-09-20 08:53:08 +0100

    cache bust loaders for FX fix

--- native/feed-noflicker.js
+++ native/feed-noflicker.js
@@ -7,7 +7,7 @@
   function loadExtra(src, attr) {
     if (document.querySelector('script[' + attr + ']')) return;
     var s = document.createElement('script');
-    s.src = src + (src.indexOf('?') >= 0 ? '&' : '?') + 'v=20260919oculos';
+    s.src = src + (src.indexOf('?') >= 0 ? '&' : '?') + 'v=20260920fx2';
     s.defer = true;
     s.setAttribute(attr, '1');
     (document.head || document.documentElement).appendChild(s);

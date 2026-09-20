cc3a08e95cd11305c1ed2ed138bd03f1471bf707
Zacarias Guilherme João <zacariasguilhermejoao@gmail.com>
2026-09-20 09:14:53 +0100

    cache bust dog FX

--- native/stable-fix.js
+++ native/stable-fix.js
@@ -3,7 +3,7 @@
  */
 (function () {
   "use strict";
-  var v = "fxLensBlack1";
+  var v = "fxCao1";
   function load(src, cb) {
     var s = document.createElement("script");
     s.src = src + (src.indexOf("?") >= 0 ? "&" : "?") + "v=" + v;

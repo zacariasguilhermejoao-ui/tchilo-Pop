15ed7d100fbfc70ca3407e02c77d60f1acb5a042
Zacarias Guilherme João <zacariasguilhermejoao@gmail.com>
2026-09-20 10:41:15 +0100

    cache bust beijos rain FX

--- native/stable-fix.js
+++ native/stable-fix.js
@@ -3,7 +3,7 @@
  */
 (function () {
   "use strict";
-  var v = "fxOlhos1";
+  var v = "fxBeijos1";
   function load(src, cb) {
     var s = document.createElement("script");
     s.src = src + (src.indexOf("?") >= 0 ? "&" : "?") + "v=" + v;

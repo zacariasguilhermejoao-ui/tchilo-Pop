2dc3446695e9c7e6cc192630d22408a470369a05
Zacarias Guilherme João <zacariasguilhermejoao@gmail.com>
2026-09-20 10:27:59 +0100

    cache bust cat FX

--- native/stable-fix.js
+++ native/stable-fix.js
@@ -3,7 +3,7 @@
  */
 (function () {
   "use strict";
-  var v = "fxCao1";
+  var v = "fxGato1";
   function load(src, cb) {
     var s = document.createElement("script");
     s.src = src + (src.indexOf("?") >= 0 ? "&" : "?") + "v=" + v;

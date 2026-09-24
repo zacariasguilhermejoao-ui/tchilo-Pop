fc4a770d6e626897a500b514fdc9065c5fa2ed53
Zacarias Guilherme João <zacariasguilhermejoao@gmail.com>
2026-09-24 17:54:09 +0100

    Load hide-nav: no bottom bar on settings

--- native/feed-stable.js
+++ native/feed-stable.js
@@ -154,5 +154,6 @@
     add('native/tchilo-avatar-fix.js?v=2');
     add('native/tchilo-profile-photos.js?v=3');
     add('native/tchilo-avatar-add.js?v=1');
+    add('native/tchilo-hide-nav.js?v=1');
   } catch (e) {}
 })();
